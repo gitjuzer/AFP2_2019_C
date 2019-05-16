76bacd86a2ffe5f996e24931ae3c477c99733fbd
Krisztián Gajdár <gajdikuka00@gmail.com>
2019-05-16 22:28:21 +0200

    React weboldal, kész index és impresszum oldalakkal, kész stílussal
    React install, index és impressum oldal tartalommal, stylussal kész, működik.
    npm-el indítható command line-ből
    
    ...sok a file

---
+++ Web/Web/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      document.getElementById('app')
+    );
+  }
+}
+
+export default App;
